633f71cb76638fe05e67ed22d8d980b2351505a5
9QIX <kirkhendrixbulaong@outlook.com>
2023-08-23 02:41:33 +0800

    script

--- MacARoo.py
+++ MacARoo.py
@@ -2,6 +2,6 @@
 
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
