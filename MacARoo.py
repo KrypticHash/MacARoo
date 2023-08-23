e00d2b1c76db3f178f809eecfcd965cdc28bca85
Kyuery <kirkhendrixbulaong@gmail.com>
2023-08-23 10:44:03 +0800

    script

--- MacARoo.py
+++ MacARoo.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 import subprocess
@@ -6,7 +7,7 @@ import optparse
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-i", "--mac", dest="new_mac", help="New MAC address" )
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address" )
 
 (options, arguments) = parser.parse_args()
 
